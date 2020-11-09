57ee45fa72ca451389b30dddce4f3a6f4d02509d
n.pankov <pankoff2000@gmail.com>
2020-11-09 12:44:45 +0100

    step 3. update crud, update table,add sort

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { DataService } from './data.service';
-import { Expenses } from './Expenses';
-import { DataSource } from '@angular/cdk/table';
-import { Observable } from 'rxjs/Observable';
-import { DialogComponent } from './dialog/dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { ViewChild, AfterViewInit } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {DataService} from './data.service';
+import {Expenses} from './Expenses';
+import {DataSource} from '@angular/cdk/table';
+import {Observable} from 'rxjs/Observable';
+import {DialogComponent} from './dialog/dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -17,27 +16,27 @@ export class AppComponent implements AfterViewInit {
   constructor(private dataService: DataService, public dialog: MatDialog) {
   }
 
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-
-  ngAfterViewInit() {
-    (this.dataSource as any).sort = this.sort;
-  }
-
   displayedColumns = ['date_expenses', 'title', 'prix', 'category', 'actions'];
   dataSource = new ExpenseDataSource(this.dataService);
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
-  deleteExpense(index) {
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  deleteExpense(index): void {
     this.dataService.deleteExpense(index);
-    this.dataSource = new ExpenseDataSource(this.dataService);
+    // this.dataSource.data = this.dataService.ELEMENT_DATA;
   }
 
   editExpense(expenses: Expenses): void {
-    let dialogRef = this.dialog.open(DialogComponent, {
+    const dialogRef = this.dialog.open(DialogComponent, {
       width: '600px',
       data: {
-        title: 'Test',
-        expenses
-      }, 
+        title: 'Modifier une dépense',
+        expenses,
+        categories: this.dataService.getCategories(),
+      },
     });
 
     dialogRef.componentInstance.event.subscribe(result => {
@@ -46,16 +45,21 @@ export class AppComponent implements AfterViewInit {
   }
 
   openDialog(): void {
-    let dialogRef = this.dialog.open(DialogComponent, {
+    const dialogRef = this.dialog.open(DialogComponent, {
       width: '600px',
       data: {
-        title: 'Ajouter depense'
+        title: 'Ajouter une dépense',
+        categories: this.dataService.getCategories(),
       }
     });
     dialogRef.componentInstance.event.subscribe((result) => {
       this.dataService.addExpense(result.data);
       this.dataSource = new ExpenseDataSource(this.dataService);
     });
+  }
+
+  getTotalCost() {
+    return this.dataService.ELEMENT_DATA.map(t => t.prix).reduce((acc, value) => acc + value, 0);
   }
 }
 
@@ -64,10 +68,21 @@ export class ExpenseDataSource extends DataSource<any> {
     super();
   }
 
+  private _sort: MatSort;
+
+  set sort(sort: MatSort) {
+    this._sort = sort;
+    this._sort.sortChange.subscribe((val) => {
+      this.dataService.getData(val);
+    });
+  }
+
+
   connect(): Observable<Expenses[]> {
     return this.dataService.getData();
   }
 
-  disconnect() {
+  disconnect(): void {
   }
 }
+
